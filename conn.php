cc5ed64314d5b6605362d6bcaf9b58e7e7a30cb5
livemehimanshu <46988939+livemehimanshu@users.noreply.github.com>
2025-11-24 11:53:19 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,13 @@
 <?php
-	$conn = new mysqli("localhost", "root", "", "db_task");
-	
-	if(!$conn){
-		die("Error: Cannot connect to the database");
-	}
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root'); 
+define('DB_PASS', 'your_password'); // CHANGE THIS
+define('DB_NAME', 'todo_db'); 
+
+try {
+    $conn = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Database Connection ERROR: " . $e->getMessage());
+}
 ?>
